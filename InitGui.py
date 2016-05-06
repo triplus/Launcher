eb98209221533bcda77e336a1060b560051c2fff
triplus <triplus@users.noreply.github.com>
2016-05-06 19:14:38 +0200

    On completion run the selected command
    - Run as single instance (Dock Widget)
    - Apply some sensible defaults
    - Remove & sign from command names
    - Filter out commands that can't be reliably detected
    - Detect and delete any potential commands with the same name
    - Create and fill standard model with command items (icon, text and tooltip)
    - Custom QLineEdit behavior (focus from pop-up menu) for better Launcher efficiency
    - Update model data whenever Launcher widget gets focus or when return key is pressed
    - Commands are always visible in completer menu (enabled/disabled state is respected)
    - Changing workbench is supported (for workbenches that are not disabled in Customize Workbenches)

---
+++ InitGui.py
@@ -0,0 +1,143 @@
+# Launcher widget for FreeCAD
+# Copyright (C) 2016  triplus @ FreeCAD
+#
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+
+from PySide import QtGui
+from PySide import QtCore
+
+
+def singleInstance():
+    from PySide import QtGui
+
+    mw = FreeCADGui.getMainWindow()
+
+    if mw:
+        for i in mw.findChildren(QtGui.QDockWidget):
+            if i.objectName() == "Launcher":
+                i.deleteLater()
+            else:
+                pass
+    else:
+        pass
+
+singleInstance()
+
+
+def dockWidget():
+    from PySide import QtGui
+    from PySide import QtCore
+
+    mw = FreeCADGui.getMainWindow()
+
+    class LauncherEdit(QtGui.QLineEdit):
+        def __init__(self, parent=None):
+            super(LauncherEdit, self).__init__(parent)
+
+        def focusInEvent(self, e):
+            if e.reason() == QtCore.Qt.PopupFocusReason:
+                pass
+            else:
+                modelData()
+
+    completer = QtGui.QCompleter()
+    completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
+
+    edit = LauncherEdit()
+    edit.setCompleter(completer)
+
+    model = QtGui.QStandardItemModel()
+    completer.setModel(model)
+
+    widget = QtGui.QDockWidget()
+    widget.setWindowTitle("Launcher")
+    widget.setObjectName("Launcher")
+    widget.setWidget(edit)
+
+    if mw:
+        mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, widget)
+    else:
+        pass
+
+    def modelData():
+
+        actions = {}
+        duplicates = []
+
+        for i in mw.findChildren(QtGui.QAction):
+            if i.objectName() is not None:
+                if i.objectName() != "":
+                    if i.objectName() in actions:
+                        if i.objectName() not in duplicates:
+                            duplicates.append(i.objectName())
+                        else:
+                            pass
+                    else:
+                        actions[i.objectName()] = i
+                else:
+                    pass
+            else:
+                pass
+
+        for d in duplicates:
+            del actions[d]
+
+        rows = len(actions)
+
+        model.clear()
+        model.setRowCount(rows)
+        model.setColumnCount(1)
+
+        row = 0
+
+        for i in actions:
+
+            item = QtGui.QStandardItem()
+            item.setText(str(actions[i].text()).replace("&", ""))
+            item.setIcon(actions[i].icon())
+            item.setToolTip(actions[i].toolTip())
+            item.setEnabled(actions[i].isEnabled())
+            item.setData(actions[i].objectName(), QtCore.Qt.UserRole)
+            model.setItem(row, 0, item)
+            row = row + 1
+
+    def onReturnPressed():
+
+        edit.clear()
+        modelData()
+
+    edit.returnPressed.connect(onReturnPressed)
+
+    def onCompleter(modelIndex):
+
+        actions = {}
+
+        for i in mw.findChildren(QtGui.QAction):
+            actions[i.objectName()] = i
+
+        index = completer.completionModel().mapToSource(modelIndex)
+        item = model.itemFromIndex(index)
+        data = item.data(QtCore.Qt.UserRole)
+
+        if data in actions:
+            actions[data].trigger()
+        else:
+            pass
+
+    completer.activated[QtCore.QModelIndex].connect(onCompleter)
+
+dockWidget()
